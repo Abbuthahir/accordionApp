d8ff46570e86aae1ce921064874d5224c7bb906f
Abbuthahir <abbuthahir04@gmail.com>
2023-08-11 11:35:49 +0530

    comit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonAccordionGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonAccordionGroup) accordionGroup!: IonAccordionGroup;
 
-  constructor() {}
+  constructor() { }
 
+  closeAccordion() {
+    this.accordionGroup.value = '';
+  }
+  toggleSection() {
+    this.accordionGroup.value = 'pulsar';
+  }
 }
